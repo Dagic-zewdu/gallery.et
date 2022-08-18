29ad7fb7ffab22c5691d1fe47175e5fec4d92c8a
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-18 12:15:12 +0300

    Added gallery reducer

---
+++ redux/reducer/gallery.js
@@ -0,0 +1,49 @@
+import galleryActionType from '../type/gallery'
+
+const INITIAL_STATE={
+    galleries: {
+        loading: true,
+        data: [],
+        error: false,
+        page:1
+    },
+gallery :{
+    loading: true,
+    data: null,
+    error: false
+}
+}
+
+export const GalleryReducer=(state=INITIAL_STATE,{type,payload})=>{
+    switch(type){
+       case galleryActionType.galleries.FETCH_GALLERIES_START:
+        return {
+            ...state,
+            galleries:{
+                ...state.galleries,
+                loading:true
+            }
+        }
+        case galleryActionType.galleries.FETCH_GALLERIES_SUCCESS:
+        return {
+            ...state,
+            galleries:{
+                ...state.galleries,
+                loading:false,
+                data:payload.data,
+                page: payload.page
+            }    
+        }
+        case galleryActionType.galleries.FETCH_GALLERIES_ERROR:
+        return {
+            ...state,
+            galleries:{
+                ...state.galleries,
+                loading:false,
+                error:false
+            }    
+        }
+      default: 
+      return state  
+    }
+}
