95ae04754ae55d06d017c0e62b5a678f24f41ba2
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-18 13:39:00 +0300

    connected redux with react

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import BottomNavigator from './components/navbar/BottomNavigator';
-import Navigator from './Navigation';
-import {store} from "./redux/rootReducer"
+import {store,persistor} from "./redux/rootReducer"
+import { PersistGate } from 'redux-persist/integration/react';
+import {Provider} from "react-redux"
 
 export default function App() {
   return (
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BottomNavigator />
+     </PersistGate>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
