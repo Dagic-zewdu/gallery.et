dff1672df07bc713343aef7e92d7d549f2db9333
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-23 22:33:13 +0300

    changed home carousel file component

---
+++ components/Home/carousel.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Dimensions, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import Carousel from 'react-native-reanimated-carousel';
+
+const HomeCarousel = () => {
+    const { galleries } = useSelector(state => state.gallery)
+
+    const width = Dimensions.get('window').width;
+    const [images, setImages] = useState([])
+    const { data } = galleries
+    useEffect(() => {
+        setImages(data?.map(({ fileurl }) => fileurl))
+    }, [data])
+    return (
+        <View>
+            {
+                <Carousel
+                    loop
+                    width={width}
+                    height={width / 2}
+                    autoPlay={true}
+                    data={[...new Array(6).keys()]}
+                    panGestureHandlerProps={{
+                        activeOffsetX: [-10, 10],
+                    }}
+                    scrollAnimationDuration={1000}
+                    renderItem={({ index }) => (
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                borderWidth: 0.1,
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Image
+                                source={{ uri: images[index + 3] }}
+                                style={{ height: "100%", width: "100%" }}
+                            />
+                        </TouchableOpacity>
+                    )}
+                />
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeCarousel;
--- screen/Home.js
+++ screen/Home.js
@@ -1,22 +1,23 @@
-import React, { useEffect }  from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import AllContainer from '../components/containers/AllContainer';
 import HomeContainer from '../components/Home';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGalleriesAsync } from '../redux/actions/gallery/galleries';
 
-const Home = ({}) => {
-    const dispatch=useDispatch()
-    useEffect(()=>{
-      dispatch(fetchGalleriesAsync())  
-    },[])
-   const {gallery}=useSelector(state=>state)
-    const {loading,error}=gallery
-    return (
-       <AllContainer>
-        <HomeContainer />
-       </AllContainer>
-    );
+const Home = ({ }) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchGalleriesAsync())
+  }, [])
+
+  const { gallery } = useSelector(state => state)
+  const { loading, error } = gallery
+  return (
+    <AllContainer>
+      <HomeContainer />
+    </AllContainer>
+  );
 }
 
 const styles = StyleSheet.create({})
